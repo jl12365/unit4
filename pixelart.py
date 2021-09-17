1a3b2f557ade6713995d17ebc2df1c91ab17c029
jl12365 <jasonli12365@gmail.com>
2021-09-17 17:20:00 -0400

    a

---
+++ pixelart.py
@@ -0,0 +1,8 @@
+import turtle
+
+Pixel_size= 30
+rows= 20
+columns= 20
+
+def initalization():
+    return None
--- threshold_test.py
+++ threshold_test.py
@@ -2,12 +2,11 @@ import squares
 
 def within_threshold(a, b, threshold):
 
-    if abs(a - b) > threshold:
+    if abs(a - b) > 0:
         return True
     elif threshold < 0:
         return None
     else :
         return False
-
 
     
